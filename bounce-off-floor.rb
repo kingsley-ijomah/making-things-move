73a616712408bad115bf46c46c6b2eaf535704c8
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-02-02 20:27:55 +0000

    Add method for dropping and bouncing

--- bounce-off-floor.rb
+++ bounce-off-floor.rb
@@ -1,17 +1,37 @@
 require 'gosu'
 
 class Ball
+	attr_accessor :x, :y, :vy, :dropping
+
 	def initialize(window)
 		@window = window
 		@width = 50
 		@height = 50
 		@image = Gosu::Image.new(@window, 'media/ball.png', true)
-		@ball_x = @window.stage_width / 2
-		@ball_y = @window.stage_height / 2
+		@x = @window.stage_width / 2
+		@y = @window.stage_height / 2
+		@vy = 1
+		@dropping = true
 	end
 
 	def draw
-		@image.draw_rot(@ball_x, @ball_y, 0, 0)
+		@image.draw_rot(@x, @y, 0, 0)
+	end
+
+	def bounce
+		@y -= @vy
+	end
+
+	def drop
+		@y += @vy
+	end
+
+	def move
+		if @dropping
+			drop
+		else
+			bounce
+		end
 	end
 end
 
@@ -23,13 +43,31 @@ class Game < Gosu::Window
 		@stage_height = 600
 		super @stage_width, @stage_height, false
 		@ball = Ball.new(self)
+		@start = false
 	end
 
 	def update
+		if @start
+			@ball.move
+		end
+
+		if @ball.y >= @stage_height then
+			@ball.dropping = false
+		end
 	end
 
 	def draw
 		@ball.draw
+	end
+
+	def button_down(id)
+		if id == Gosu::KbEscape then
+			close
+		end
+
+		if id == Gosu::KbSpace then
+			@start = true
+		end
 	end
 end
 
