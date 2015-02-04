5413051271fb41e843d565e4185f24028f1934ef
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-02-04 18:34:50 +0000

    Add ball class and image

--- bounce.rb
+++ bounce.rb
@@ -1,17 +1,38 @@
 require 'gosu'
 
+class Ball
+  def initialize(win)
+    @w = 50
+    @h = 50
+    @x = 200
+    @y = 200
+    @speed = 1
+    @image = Gosu::Image.new(win, 'media/ball.png', true)
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, 0, 0)
+  end
+
+  def move
+    @x += @speed
+  end
+end
+
 class Game < Gosu::Window
   def initialize
     @width = 800
     @height = 400
     super @width, @height, false
+    @ball = Ball.new(self)
   end
 
   def update
-    puts 'hello'
+    @ball.move
   end
 
   def draw
+    @ball.draw
   end
 end
 
