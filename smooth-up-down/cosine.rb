f87ab06954d5c4dd3d21ca00b6dd824a8c9eec81
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-02-07 18:52:24 +0000

    Add sine and cosine test files

---
+++ smooth-up-down/cosine.rb
@@ -0,0 +1,5 @@
+angle = 0
+while angle < Math::PI * 2
+  puts Math.cos(angle)
+  angle += 0.1
+end
---
+++ smooth-up-down/sine.rb
@@ -0,0 +1,5 @@
+angle = 0
+while angle < Math::PI * 4
+  puts Math.sin(angle)
+  angle += 0.1
+end
