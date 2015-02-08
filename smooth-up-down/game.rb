0606d2e76b390b8fa1c4265bb699f561ae2d8bea
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-02-08 20:30:15 +0000

    Adding game and ball file initialize codes

---
+++ smooth-up-down/game.rb
@@ -0,0 +1,24 @@
+require 'gosu'
+require './ball'
+
+class Game < Gosu::Window
+  attr_reader :w, :h
+
+  def initialize
+    @w = 600
+    @h = 600
+    super @w, @h, false
+    self.caption = 'Smooth up and down'
+    @ball = Ball.new(self)
+  end
+
+  def update
+  end
+
+  def draw
+    @ball.draw
+  end
+end
+
+game = Game.new
+game.show
