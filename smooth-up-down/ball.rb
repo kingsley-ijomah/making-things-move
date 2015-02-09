ba98b77817a53978f7eca5c80c3044c745a0edd9
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-02-09 15:31:15 +0000

    Add bounce method

--- smooth-up-down/ball.rb
+++ smooth-up-down/ball.rb
@@ -5,10 +5,18 @@ class Ball
     @h = 50
     @x = @win.w/2
     @y = @win.h/2
+    @centerY = @win.h/2
+    @angle = 0
+    @range = 275
     @image = Gosu::Image.new(@win, '../media/ball.png', false)
   end
 
   def draw
     @image.draw_rot(@x, @y, 0, 0)
+  end
+
+  def bounce
+    @y = @centerY + Math.sin(@angle) * @range
+    @angle += 0.05
   end
 end
